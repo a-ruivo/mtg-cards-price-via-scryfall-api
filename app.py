3d87c13a787fc9ca71cdb95f1a8342a77025d702
a-ruivo <allanruivo@outlook.com>
2025-08-13 21:10:04 -0300

    a

--- app.py
+++ app.py
@@ -290,14 +290,15 @@ elif st.session_state["aba_atual"] == "Dashboard":
 
     # Ícones de mana (SVGs hospedados)
     mana_icons = {
-        "W": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/5/5f/W.svg",
-        "U": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/3/3f/U.svg",
-        "B": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/2/2f/B.svg",
-        "R": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/6/6f/R.svg",
-        "G": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/4/4f/G.svg",
-        "C": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/8/8f/C.svg",
-        "L": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f4/L.svg"
+        "W": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/8/8e/W.svg/revision/latest?cb=20160125094923",
+        "U": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/9/9f/U.svg/revision/latest?cb=20160121092256",
+        "B": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/2/2f/B.svg",  # já estava funcionando
+        "R": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/8/87/R.svg/revision/latest?cb=20160125094913",
+        "G": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/8/88/G.svg/revision/latest?cb=20160125094907",
+        "C": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/1/1a/C.svg/revision/latest?cb=20160121092204",
+        "L": "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f4/L.svg"  # já estava funcionando
     }
+
 
     fig1 = go.Figure()
 
